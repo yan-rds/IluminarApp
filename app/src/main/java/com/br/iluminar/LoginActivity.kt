cec01945f2fe3eee3d95041c908df6d98a87a316
Yan <yyan1207@gmail.com>
2022-12-29 17:07:07 -0300

    Authenticate Login with FireBase

--- app/src/main/java/com/br/iluminar/LoginActivity.kt
+++ app/src/main/java/com/br/iluminar/LoginActivity.kt
@@ -1,12 +1,77 @@
 package com.br.iluminar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.br.iluminar.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        setupListener()
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null){
+            goToHubActivity()
+        }
+    }
+
+    private fun setupListener(){
+        binding.loginBt.setOnClickListener {
+            val email:String = binding.emailEt.text.toString()
+            val password:String = binding.passwordEt.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            } else {
+                authUser()
+            }
+        }
+    }
+
+    private fun goToHubActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun authUser(){
+        val email:String = binding.emailEt.text.toString()
+        val password:String = binding.passwordEt.text.toString()
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                binding.loginProgressBar.visibility = View.VISIBLE
+                Handler(Looper.getMainLooper()).postDelayed(
+                    {
+                       goToHubActivity()
+                    }, 3000
+                )
+            } else {
+                try {
+                    throw task.exception!!
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Usuário ou senha inválidos", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
 }
