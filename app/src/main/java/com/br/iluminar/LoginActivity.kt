0162596f9ec972c7e5767bcb5b0e5473207a880d
Yan <yyan1207@gmail.com>
2023-01-04 16:08:37 -0300

    fix loginActivity

--- app/src/main/java/com/br/iluminar/LoginActivity.kt
+++ app/src/main/java/com/br/iluminar/LoginActivity.kt
@@ -52,6 +52,7 @@ class LoginActivity : AppCompatActivity() {
 
     private fun goToHubActivity(){
         val intent = Intent(this, HubActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
         startActivity(intent)
     }
 
